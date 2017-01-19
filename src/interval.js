19fa7d6e7aa2b7130dfb0935d6218ccf6c670de2
smhg <sam@strictlyphp.com>
2017-01-19 11:04:33 +0100

    use semistandard style

--- src/interval.js
+++ src/interval.js
@@ -3,27 +3,27 @@
 import createDuration from 'date-duration';
 
 const toDate = value => {
-    if (typeof value === 'object' && typeof value.toDate === 'function') {
-      return value.toDate();
-    }
+  if (typeof value === 'object' && typeof value.toDate === 'function') {
+    return value.toDate();
+  }
 
-    if (typeof value === 'string' && value[0] !== 'P') {
-      return new Date(value);
-    }
+  if (typeof value === 'string' && value[0] !== 'P') {
+    return new Date(value);
+  }
 
-    return value;
-  },
-  durationToDate = (value, idx, all) => {
-    if (typeof value === 'string' && value[0] === 'P') {
-      if (idx === 0) {
-        return createDuration(value).subtractFrom(all[1]);
-      } else if (idx === 1) {
-        return createDuration(value).addTo(all[0]);
-      }
+  return value;
+};
+const durationToDate = (value, idx, all) => {
+  if (typeof value === 'string' && value[0] === 'P') {
+    if (idx === 0) {
+      return createDuration(value).subtractFrom(all[1]);
+    } else if (idx === 1) {
+      return createDuration(value).addTo(all[0]);
     }
+  }
 
-    return value;
-  };
+  return value;
+};
 
 export default function createInterval () {
   if (arguments.length === 0 || !arguments[0]) {
@@ -35,68 +35,77 @@ export default function createInterval () {
     .map(durationToDate)
     .map(value => +value);
 
-  const toString = () => `${(new Date(start)).toISOString()}/${(new Date(end)).toISOString()}`,
-    overlaps = interval => start < interval.end && interval.start < end,
-    union = function (interval) {
-      if (overlaps(interval) || end === interval.start || interval.end === start) {
-        return [createInterval(start <= interval.start ? start : interval.start, end >= interval.end ? end : interval.end)];
-      }
+  const toString = () => `${(new Date(start)).toISOString()}/${(new Date(end)).toISOString()}`;
 
-      return [createInterval(start, end), createInterval(interval.start, interval.end)];
-    },
-    diff = function (interval) {
-      if (!overlaps(interval)) {
-        return [createInterval(start, end)];
-      }
+  const overlaps = interval => start < interval.end && interval.start < end;
 
-      if (interval.start <= start) {
-        // start before/together
-        if (end > interval.end) {
-          // end inside
-          return [createInterval(interval.end, end)];
-        }
+  const union = function (interval) {
+    if (overlaps(interval) || end === interval.start || interval.end === start) {
+      return [createInterval(start <= interval.start ? start : interval.start, end >= interval.end ? end : interval.end)];
+    }
 
-        // end after/together
-        return [];
-      }
+    return [createInterval(start, end), createInterval(interval.start, interval.end)];
+  };
 
-      // start inside
-      if (interval.end < end) {
+  const diff = function (interval) {
+    if (!overlaps(interval)) {
+      return [createInterval(start, end)];
+    }
+
+    if (interval.start <= start) {
+      // start before/together
+      if (end > interval.end) {
         // end inside
-        return [createInterval(start, interval.start), createInterval(interval.end, end)];
+        return [createInterval(interval.end, end)];
       }
 
       // end after/together
-      return [createInterval(start, interval.start)];
-    },
-    intersection = function (interval) {
-      if (!overlaps(interval)) {
-        return;
-      }
+      return [];
+    }
 
-      if (interval.start <= start) {
-        // start before/together
-        if (end > interval.end) {
-          // end inside
-          return createInterval(start, interval.end);
-        }
+    // start inside
+    if (interval.end < end) {
+      // end inside
+      return [createInterval(start, interval.start), createInterval(interval.end, end)];
+    }
 
-        // end after/together
-        return createInterval(start, end);
-      }
+    // end after/together
+    return [createInterval(start, interval.start)];
+  };
 
-      // start inside
-      if (interval.end < end) {
+  const intersection = function (interval) {
+    if (!overlaps(interval)) {
+      return;
+    }
+
+    if (interval.start <= start) {
+      // start before/together
+      if (end > interval.end) {
         // end inside
-        return createInterval(interval.start, interval.end);
+        return createInterval(start, interval.end);
       }
 
       // end after/together
-      return createInterval(interval.start, end);
-    };
+      return createInterval(start, end);
+    }
+
+    // start inside
+    if (interval.end < end) {
+      // end inside
+      return createInterval(interval.start, interval.end);
+    }
+
+    // end after/together
+    return createInterval(interval.start, end);
+  };
 
   return Object.freeze({
-    start, end,
-    toString, overlaps, union, diff, intersection
+    start,
+    end,
+    toString,
+    overlaps,
+    union,
+    diff,
+    intersection
   });
 }
--- test/interval.js
+++ test/interval.js
@@ -44,8 +44,8 @@ describe('interval', () => {
     });
 
     it('should accept a start and end date', () => {
-      let start = new Date(),
-        end = new Date();
+      let start = new Date();
+      let end = new Date();
 
       end.setUTCDate(end.getUTCDate() + 1);
 
@@ -63,19 +63,19 @@ describe('interval', () => {
 
   describe('#overlaps()', () => {
     it('should detect overlapping intervals', () => {
-      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z',
-        iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z',
-        iso3 = '2015-08-06T12:00:00.000Z/2015-08-07T12:00:00.000Z',
-        iso4 = '2015-08-07T12:00:00.000Z/2015-08-10T12:00:00.000Z';
+      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z';
+      let iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z';
+      let iso3 = '2015-08-06T12:00:00.000Z/2015-08-07T12:00:00.000Z';
+      let iso4 = '2015-08-07T12:00:00.000Z/2015-08-10T12:00:00.000Z';
 
       assert(createInterval(iso1).overlaps(createInterval(iso2)));
       assert(!createInterval(iso1).overlaps(createInterval(iso3)));
       assert(!createInterval(iso3).overlaps(createInterval(iso4)));
       assert(!createInterval(iso4).overlaps(createInterval(iso3)));
 
-      let iso5 = '2015-10-25T02:00:00+01:00/2015-10-25T03:00:00+01:00',
-        iso6 = '2015-10-25T01:00:00+02:00/2015-10-25T02:00:00+02:00',
-        iso7 = '2015-10-25T01:00:00+02:00/2015-10-25T03:00:00+01:00';
+      let iso5 = '2015-10-25T02:00:00+01:00/2015-10-25T03:00:00+01:00';
+      let iso6 = '2015-10-25T01:00:00+02:00/2015-10-25T02:00:00+02:00';
+      let iso7 = '2015-10-25T01:00:00+02:00/2015-10-25T03:00:00+01:00';
 
       assert(!createInterval(iso5).overlaps(createInterval(iso6)));
       assert(createInterval(iso5).overlaps(createInterval(iso7)));
@@ -84,14 +84,14 @@ describe('interval', () => {
 
   describe('#union()', () => {
     it('should create union arrays', () => {
-      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z',
-        iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z';
+      let iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T12:00:00.000Z';
 
       assert(createInterval(iso1).union(createInterval(iso2)).length === 1);
 
-      let iso3 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z',
-        iso4 = '2015-08-04T18:00:00.000Z/2015-08-05T12:00:00.000Z',
-        iso5 = '2015-08-04T12:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let iso3 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z';
+      let iso4 = '2015-08-04T18:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let iso5 = '2015-08-04T12:00:00.000Z/2015-08-05T12:00:00.000Z';
 
       assert(createInterval(iso3).union(createInterval(iso4)).length === 2);
       assert(createInterval(iso3).union(createInterval(iso5)).length === 1);
@@ -100,16 +100,16 @@ describe('interval', () => {
 
   describe('#diff()', () => {
     it('should create difference arrays', () => {
-      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z',
-        iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T12:00:00.000Z',
-        diff1 = createInterval(iso1).diff(createInterval(iso2));
+      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z';
+      let iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let diff1 = createInterval(iso1).diff(createInterval(iso2));
 
       assert(diff1.length === 1);
       assert.equal(diff1[0].toString(), '2015-08-03T12:00:00.000Z/2015-08-04T00:00:00.000Z');
 
-      let iso3 = '2015-08-03T12:00:00.000Z/2015-08-05T12:00:00.000Z',
-        iso4 = '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z',
-        diff2 = createInterval(iso3).diff(createInterval(iso4));
+      let iso3 = '2015-08-03T12:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let iso4 = '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z';
+      let diff2 = createInterval(iso3).diff(createInterval(iso4));
 
       assert(diff2.length === 2);
       assert.equal(diff2[1].toString(), '2015-08-05T00:00:00.000Z/2015-08-05T12:00:00.000Z');
@@ -118,21 +118,21 @@ describe('interval', () => {
 
   describe('#intersection()', () => {
     it('should create intersection interval', () => {
-      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z',
-        iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T12:00:00.000Z',
-        intersection1 = createInterval(iso1).intersection(createInterval(iso2));
+      let iso1 = '2015-08-03T12:00:00.000Z/2015-08-04T12:00:00.000Z';
+      let iso2 = '2015-08-04T00:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let intersection1 = createInterval(iso1).intersection(createInterval(iso2));
 
       assert.equal(intersection1.toString(), '2015-08-04T00:00:00.000Z/2015-08-04T12:00:00.000Z');
 
-      let iso3 = '2015-08-03T12:00:00.000Z/2015-08-05T12:00:00.000Z',
-        iso4 = '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z',
-        intersection2 = createInterval(iso3).intersection(createInterval(iso4));
+      let iso3 = '2015-08-03T12:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let iso4 = '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z';
+      let intersection2 = createInterval(iso3).intersection(createInterval(iso4));
 
       assert.equal(intersection2.toString(), '2015-08-04T00:00:00.000Z/2015-08-05T00:00:00.000Z');
 
-      let iso5 = '2015-08-03T12:00:00.000Z/2015-08-05T12:00:00.000Z',
-        iso6 = '2015-08-07T00:00:00.000Z/2015-08-10T00:00:00.000Z',
-        intersection3 = createInterval(iso5).intersection(createInterval(iso6));
+      let iso5 = '2015-08-03T12:00:00.000Z/2015-08-05T12:00:00.000Z';
+      let iso6 = '2015-08-07T00:00:00.000Z/2015-08-10T00:00:00.000Z';
+      let intersection3 = createInterval(iso5).intersection(createInterval(iso6));
 
       assert.equal(typeof intersection3, 'undefined');
     });
