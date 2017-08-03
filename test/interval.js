2f927cb6759df672ae5f4e69b2d5be5b2eefdc4c
smhg <sam@strictlyphp.com>
2017-08-03 19:36:17 +0200

    include polyfills in build

--- test/interval.js
+++ test/interval.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import assert from 'assert';
-import createInterval from '../dist/interval';
+import createInterval from '../src/interval';
 
 describe('interval', () => {
   describe('#()', () => {
